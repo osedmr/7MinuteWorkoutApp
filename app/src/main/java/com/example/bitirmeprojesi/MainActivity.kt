285702f288b855277924ba7503197a5e4d933e2c
osman <osefe47@gmail.com>
2024-07-06 21:31:32 +0300

    7MinuteWorkoutApp

---
+++ app/src/main/java/com/example/bitirmeprojesi/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.bitirmeprojesi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.bitirmeprojesi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.flBMI?.setOnClickListener {
+            // Launching the BMI Activity
+            val intent = Intent(this, BMIActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        binding?.flHistory?.setOnClickListener {
+
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
+
+
+
+
+
